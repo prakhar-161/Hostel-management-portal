9dd24f849033cbc9b3309956dc45ee24c8294c56
Prakhar Dhama <pdhama161@gmail.com>
2022-07-01 00:10:26 +0530

    server and folder setup

--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,28 @@
+require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
-
+const mongoose = require('mongoose');
 
 const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+const port = process.env.PORT || 5200;
+const uri = process.env.MONGO_URI;
+
+app.listen(port, (err) => {
+    if(err) { console.log(err.message); }
+    console.log(`Connected to port ${port}`);
+});
+
+mongoose.connect(uri, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+})
+.then(() => {
+    console.log('Successfully connected to MongoDB');
+})
+.catch((err) => {
+    console.log(err.message);
+});
